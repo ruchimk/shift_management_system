32aaa56116dce45fb0e7aa235c4c3688ed8f14e5
Ruchi Malik <rmalik2788@gmail.com>
2014-11-14 11:44:35 -0500

    created user controller and added the sendgrid config settings to production.rb file in environments folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'your_sendgrid_username',
+  :password => 'your_sendgrid_password',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 end
