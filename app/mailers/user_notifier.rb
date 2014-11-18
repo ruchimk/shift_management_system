2cd04691e8c32a3f9404b10643bf46abbdc5dd33
Ruchi Malik <rmalik2788@gmail.com>
2014-11-17 20:35:14 -0500

    commented out the user_notifier methods that aren’t working yet(waiting on all relationships to be formed)

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -9,25 +9,25 @@ class UserNotifier < ActionMailer::Base
     sent_on: Time.now )
   end
 
-# send an email to all admins when user requests shift change
-  def shiftchange_request_email(user)
-    @user = user
-    mail(to: user.company.users.where(is_admin:true) },
-    subject: "#{@user.first_name} Requested a Shift Change" )
-  end
+# # send an email to all admins when user requests shift change
+#   def shiftchange_request_email(user)
+#     @user = user
+#     mail(to: user.company.users.where(is_admin:true) },
+#     subject: "#{@user.first_name} Requested a Shift Change" )
+#   end
 
-# send a response email to user by admins when user requests shift change
-  def shiftchange_response_email(user)
-    @user = user
-    mail( to: user.email,
-    subject: 'Status update regarding shift change request' )
-  end
+# # send a response email to user by admins when user requests shift change
+#   def shiftchange_response_email(user)
+#     @user = user
+#     mail( to: user.email,
+#     subject: 'Status update regarding shift change request' )
+#   end
 
-  # send a reassignment email to user by admins when user requests shift change
-  def shift_reassign_email(user)
-    @user = user
-    mail( to: user.email,
-    from: user.company.users.where(is_admin:true) },
-    subject: 'You have been reassigned a shift, please check your schedule' )
-  end
+#   # send a reassignment email to user by admins when user requests shift change
+#   def shift_reassign_email(user)
+#     @user = user
+#     mail( to: user.email,
+#     from: user.company.users.where(is_admin:true) },
+#     subject: 'You have been reassigned a shift, please check your schedule' )
+#   end
 end
