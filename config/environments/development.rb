94da84a417871f784ff0502eb7e430f77a65fd42
Ruchi Malik <rmalik2788@gmail.com>
2014-11-22 14:36:10 -0500

    trying to add new s3 bucket

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,15 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
   #specify what domain to use for mailer URLs
   config.action_mailer.default_url_options = {host: "localhost:3000"}
   config.action_mailer.perform_deliveries = true
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,20 +1,10 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options.merge!({
-  storage: :s3,
+  :storage => :s3,
   :s3_credentials => {
-      :bucket => 'shiftable',
-      :access_key_id => 'AKIAI27H6KM4IGJ7AWKQ',
-      :secret_access_key => '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'
-    }
-  })
-
-# config.paperclip_defaults = {
-#   storage: :s3,
-#   s3_credentials: {
-#     :access_key_id => 'AKIAI27H6KM4IGJ7AWKQ',
-#     :secret_access_key => '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',
-#     :bucket => 'shiftable'
-#   }
-# }
-
-  # s3_host_name: 's3-us-west-1.amazonaws.com',
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
+  }
+})
